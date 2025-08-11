f9db89fb2bfeb6797af145850aa217bb2735e130
lo-vi1671 <anusha.sharma_cs24@gla.ac.in>
2025-08-11 11:36:00 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,24 @@
+public class Datatype{
+    public static void main(String[] args) {
+        int a = 6;
+        float rad = 5.6f;
+        double r = 23.78;
+        char gr = 'C';
+        boolean isValid = true;
+        byte c = 99;
+        short v = 4689;
+        long l = 478324932;
+        System.out.println(a);
+        System.out.println(rad);
+        System.out.println(r);
+        System.out.println(gr);
+        System.out.println(isValid);
+        System.out.println(c);
+        System.out.println(v);
+        System.out.println(l);
+
+
+
+
+    }
+}
