864eb211c6ed95228fa4fe69acdf44fcaa7590a8
lo-vi1671 <anusha.sharma_cs24@gla.ac.in>
2025-08-11 11:03:43 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,24 @@
+public class Variable {
+    public static void main(String[] args) {
+        String $delisha = "who gives pleasure";
+        int b = 8;
+        String diyaSha = "good student";
+        System.out.println($delisha);
+        System.out.println(b);
+        System.out.println(diyaSha);
+
+
+        int a = 3;
+        int v = 4;
+        int sum = a + v;
+        System.out.println(sum);
+
+        System.out.println(2+3);
+
+        
+
+
+
+    }
+    
+}
