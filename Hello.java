18c6069385cd3541943a55baaf8557fbfc848acd
lo-vi1671 <anusha.sharma_cs24@gla.ac.in>
2025-08-11 10:18:36 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.print("Anusha");
+        System.out.print(" Sharma");
+        /*My name is Anusha.
+         * this is my first java code.
+         */
+        
+
+    }
+    
+}
