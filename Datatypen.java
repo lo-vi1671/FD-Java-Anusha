fd8645652a4c606a659d06327117edd64e3be9ea
lo-vi1671 <anusha.sharma_cs24@gla.ac.in>
2025-08-12 08:41:08 +0530

    Add files via upload

--- Datatypen.java
+++ Datatypen.java
@@ -5,6 +5,17 @@ public class Datatypen {
         System.out.println(Krishnanagi);
         System.out.println(arr[3]);
 
+        int[]b = {3, 6, 7};
+        int[]c = {3,0,7};
+        int[]a = {3,3,7};
+        System.out.println(b[1]);
+        System.out.println(c[0]);
+        System.out.println(a[2]);
+
+
+
+
+
     }
     
 }
