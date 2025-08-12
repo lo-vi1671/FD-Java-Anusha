c9dac0569f3eb934f10eacfe8edf710fd0808dc5
lo-vi1671 <anusha.sharma_cs24@gla.ac.in>
2025-08-12 08:30:50 +0530

    Add files via upload

---
+++ Datatypen.java
@@ -0,0 +1,10 @@
+public class Datatypen {
+    public static void main(String[] args) {
+        String Krishnanagi = "female friend of Krishnji";
+        int arr[] = {5, 6, 7, 8, 9};
+        System.out.println(Krishnanagi);
+        System.out.println(arr[3]);
+
+    }
+    
+}
