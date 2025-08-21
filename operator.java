94f57cc21d528f15d0c143a60ebbba8c40e580f3
lo-vi1671 <anusha.sharma_cs24@gla.ac.in>
2025-08-21 13:44:18 +0530

    Add files via upload

---
+++ operator.java
@@ -0,0 +1,18 @@
+public class operator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        int prod = a*b;
+        int div = a/b;
+        int diff = a-b;
+        int mod = a%b;
+        System.out.println("sum: "+ sum);
+        System.out.println("prod: "+ prod);
+        System.out.println("division: "+ div);
+        System.out.println("difference: "+ diff);
+        System.out.println("modulus: "+ mod);
+        
+    }
+    
+}
