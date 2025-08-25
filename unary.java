6475ae1116dea7564cf9a771d4286412677043fe
lo-vi1671 <anusha.sharma_cs24@gla.ac.in>
2025-08-25 10:13:24 +0530

    Add files via upload

---
+++ unary.java
@@ -0,0 +1,10 @@
+public class unary {
+    public static void main(String[] args) {
+        // unary 
+        System.out.println("-a =" + (-a));
+
+        // increment operator
+        a++
+    }
+    
+}
