1403084d79297a7baf1aea1a07eac57af39d125f
lo-vi1671 <anusha.sharma_cs24@gla.ac.in>
2025-08-25 10:12:37 +0530

    Add files via upload

---
+++ binary.java
@@ -0,0 +1,11 @@
+public class binary {
+    public static void main(String[] args) {
+        // binary operators
+        int a = 10;
+        int b = 20;
+
+        System.out.println("a&b = "+ (a & b));
+        System.out.println(("a"));
+    }
+
+}
