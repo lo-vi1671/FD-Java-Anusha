f94f39279cd3fa3efc52b33288fa49b153c1bd31
lo-vi1671 <anusha.sharma_cs24@gla.ac.in>
2025-08-26 08:58:59 +0530

    Add files via upload

---
+++ explicit.java
@@ -0,0 +1,9 @@
+public class explicit {
+    public static void main(String[] args) {
+        double d = 100.99;
+        int n = (int) d;
+        System.out.println(d);
+        System.out.println(n);
+    }
+    
+}
