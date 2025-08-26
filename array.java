63f547cb4fb2790cf8f53312203d072dcfd79994
lo-vi1671 <anusha.sharma_cs24@gla.ac.in>
2025-08-26 09:48:17 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,12 @@
+public class array {
+    public static void main(String[] args) {
+        int[] arr = {4, 7, 6, 5, 8};
+        int n = arr.length;
+        System.out.println("array:");
+        for (int i = 0; i<n; i++) {
+            System.out.println(arr[i] + " ");
+        }
+
+    }
+    
+}
